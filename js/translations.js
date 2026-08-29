cae0b22857f40e90d045aea14cb6273cd9410de1
Larrea-Mon <152271612+Larrea-Mon@users.noreply.github.com>
2026-08-29 22:50:12 +0200

    Implementar sistema de traducción

---
+++ js/translations.js
@@ -0,0 +1,136 @@
+﻿// Manual translation dictionary. Edit the strings below for each language.
+window.translations = {
+     en: {
+          nav_home: "Home",
+          nav_about: "About Me",
+          nav_experience: "Experiences",
+          nav_testimonial: "Testimonial",
+          nav_contact: "Contact",
+
+          home_welcome: "welcome to my website",
+          home_title_pre: "Hello, I am",
+          home_title_post: "currently based in New York city.",
+          home_desc: "Donec auctor arcu at efficitur lacinia. Praesent bibendum efficitur ipsum, et mattis tellus interdum in. Ut a dictum purus. Vestibulum non pellentesque felis, sed dignissim urna. Vestibulum id accumsan quam.",
+          home_button: "Get Started",
+
+          about_title: "Donec auctor",
+          about_subtitle: "Sed vulputate vitae diam quis bibendum",
+          about_p1: "Phasellus vulputate tellus nec tortor varius elementum. Curabitur at pulvinar ante. Duis dui urna, faucibus eget felis eu, iaculis congue sem. Mauris convallis eros massa.",
+          about_p2: "Quisque viverra iaculis aliquam. Etiam volutpat, justo non aliquam bibendum, sem nibh mollis erat, quis porta odio odio at velit.",
+
+          skills_title: "My Skills",
+          skills_subtitle: "Photoshop . HTML CSS JS . Web Design",
+          skill_frontend: "Frontend Design",
+          skill_backend: "Backend processing",
+          skill_html: "HTML5 & CSS3",
+
+          service_1_title: "Interface Design",
+          service_1_desc: "Phasellus vulputate tellus nec tortor varius elementum. Curabitur at pulvinar ante.",
+          service_2_title: "Media Strategy",
+          service_2_desc: "Curabitur at pulvinar ante. Duis dui urna, faucibus eget felis eu, iaculis congue sem.",
+          service_3_title: "Mobile App",
+          service_3_desc: "Mauris convallis eros massa, vitae euismod arcu tempus ut. Quisque viverra iaculis.",
+          service_4_title: "Coding",
+          service_4_desc: "Mauris convallis eros massa, vitae euismod arcu tempus ut. Quisque viverra iaculis.",
+
+          experience_title: "My Experiences",
+          experience_subtitle: "Previous companies and my tasks",
+          experience_1_role: "Graphic Designer",
+          experience_1_date: "2014 Jul - 2015 Sep",
+          experience_1_desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+          experience_2_role: "Web Designer",
+          experience_2_date: "2015 Oct - 2017 Jan",
+          experience_2_desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+
+          education_title: "My Education",
+          education_subtitle: "In cursus orci non ipsum gravida dignissim",
+          education_1_role: "Master in Design",
+          education_1_date: "2012 Jan - 2014 May",
+          education_1_desc: "Etiam iaculis elit in mauris ullamcorper auctor. Proin a sapien id orci ullamcorper dignissim eu in neque.",
+          education_2_role: "Bachelor of Arts",
+          education_2_date: "2008 May - 2011 Dec",
+          education_2_desc: "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+
+          quotes_text: "Proin lobortis eu diam et facilisis. Fusce nisi nibh, molestie in vestibulum quis, auctor et orci.",
+          quotes_author: "Curabitur at pulvinar ante. Duis dui urna, faucibus eget felis eu, iaculis congue sem.",
+
+          contact_title: "Say hello..",
+          contact_subtitle: "Integer ut consectetur est. In cursus orci non ipsum gravida dignissim.",
+          contact_name_placeholder: "Your Name",
+          contact_email_placeholder: "Your Email",
+          contact_message_placeholder: "Write your message...",
+          contact_submit: "Send",
+          contact_office_title: "Visit my office",
+          contact_office_address: "456 New Street 22000, New York City, USA",
+          contact_info_title: "Contact.",
+
+          footer_copyright: "Copyright \u00A9 2016 Your Company"
+     },
+
+     es: {
+          nav_home: "Inicio",
+          nav_about: "Sobre M\u00ED",
+          nav_experience: "Experiencia",
+          nav_testimonial: "Testimonios",
+          nav_contact: "Contacto",
+
+          home_welcome: "bienvenido a mi sitio web",
+          home_title_pre: "Hola, soy",
+          home_title_post: "actualmente radicado en Nueva York.",
+          home_desc: "Donec auctor arcu at efficitur lacinia. Praesent bibendum efficitur ipsum, et mattis tellus interdum in. Ut a dictum purus. Vestibulum non pellentesque felis, sed dignissim urna. Vestibulum id accumsan quam.",
+          home_button: "Comenzar",
+
+          about_title: "Donec auctor",
+          about_subtitle: "Sed vulputate vitae diam quis bibendum",
+          about_p1: "Phasellus vulputate tellus nec tortor varius elementum. Curabitur at pulvinar ante. Duis dui urna, faucibus eget felis eu, iaculis congue sem. Mauris convallis eros massa.",
+          about_p2: "Quisque viverra iaculis aliquam. Etiam volutpat, justo non aliquam bibendum, sem nibh mollis erat, quis porta odio odio at velit.",
+
+          skills_title: "Mis Habilidades",
+          skills_subtitle: "Photoshop . HTML CSS JS . Dise\u00F1o Web",
+          skill_frontend: "Dise\u00F1o Frontend",
+          skill_backend: "Procesamiento Backend",
+          skill_html: "HTML5 y CSS3",
+
+          service_1_title: "Dise\u00F1o de Interfaz",
+          service_1_desc: "Phasellus vulputate tellus nec tortor varius elementum. Curabitur at pulvinar ante.",
+          service_2_title: "Estrategia de Medios",
+          service_2_desc: "Curabitur at pulvinar ante. Duis dui urna, faucibus eget felis eu, iaculis congue sem.",
+          service_3_title: "Aplicaci\u00F3n M\u00F3vil",
+          service_3_desc: "Mauris convallis eros massa, vitae euismod arcu tempus ut. Quisque viverra iaculis.",
+          service_4_title: "Programaci\u00F3n",
+          service_4_desc: "Mauris convallis eros massa, vitae euismod arcu tempus ut. Quisque viverra iaculis.",
+
+          experience_title: "Mi Experiencia",
+          experience_subtitle: "Empresas anteriores y mis tareas",
+          experience_1_role: "Dise\u00F1ador Gr\u00E1fico",
+          experience_1_date: "Jul 2014 - Sep 2015",
+          experience_1_desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+          experience_2_role: "Dise\u00F1ador Web",
+          experience_2_date: "Oct 2015 - Ene 2017",
+          experience_2_desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+
+          education_title: "Mi Educaci\u00F3n",
+          education_subtitle: "In cursus orci non ipsum gravida dignissim",
+          education_1_role: "M\u00E1ster en Dise\u00F1o",
+          education_1_date: "Ene 2012 - May 2014",
+          education_1_desc: "Etiam iaculis elit in mauris ullamcorper auctor. Proin a sapien id orci ullamcorper dignissim eu in neque.",
+          education_2_role: "Licenciatura en Artes",
+          education_2_date: "May 2008 - Dic 2011",
+          education_2_desc: "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+
+          quotes_text: "Proin lobortis eu diam et facilisis. Fusce nisi nibh, molestie in vestibulum quis, auctor et orci.",
+          quotes_author: "Curabitur at pulvinar ante. Duis dui urna, faucibus eget felis eu, iaculis congue sem.",
+
+          contact_title: "Saluda..",
+          contact_subtitle: "Integer ut consectetur est. In cursus orci non ipsum gravida dignissim.",
+          contact_name_placeholder: "Tu Nombre",
+          contact_email_placeholder: "Tu Correo",
+          contact_message_placeholder: "Escribe tu mensaje...",
+          contact_submit: "Enviar",
+          contact_office_title: "Visita mi oficina",
+          contact_office_address: "456 New Street 22000, Nueva York, EE.UU.",
+          contact_info_title: "Contacto.",
+
+          footer_copyright: "Copyright \u00A9 2016 Tu Empresa"
+     }
+};
